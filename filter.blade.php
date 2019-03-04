5d0179e2bb38357678b59902cb968edd8ea65824
Iwan Jeriatno <jeriat01@gmail.com>
2019-03-04 23:03:57 +0800

    Create filter.blade.php

---
+++ filter.blade.php
@@ -0,0 +1,14 @@
+<form>
+  <div class="col-lg-3">
+      <label for="">Nama Konsultan</label>
+      <input type="text" name="consultant_name" id="consultant_name" class="form-control m-input" placeholder="Nama Konsultan">
+  </div>
+  <button type="button" class="btn btn-success m-btn btn-sm 	m-btn m-btn--icon" id="btn-filter-noajax">
+      <span>
+          <i class="la la-floppy-o la-2x"></i>
+          <span>
+              Filter
+          </span>
+      </span>
+  </button>
+</form>
